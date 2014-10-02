27b28f0c50b37a1299c69a7513e9a303606c14af
Tim Lord <tim@izettle.com>
2014-10-02 07:57:37 +0200

    Adds back ANSIBLE_LIBRARY export
    Not quite sure why or if this is required but occasionally I get an
    error without it.

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,6 +5,9 @@ tee /etc/apt/sources.list.d/ansible-ansible-trusty.list >/dev/null <<EOF
 deb http://ppa.launchpad.net/ansible/ansible/ubuntu trusty main
 # deb-src http://ppa.launchpad.net/ansible/ansible/ubuntu trusty main
 EOF
+tee /etc/profile.d/ansible.sh >/dev/null <<EOF
+export ANSIBLE_LIBRARY=/usr/share/ansible
+EOF
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys "6125E2A8C77F2818FB7BD15B93C4A3FD7BB9C367"
 apt-get -y update
 apt-get -y install ansible unzip sudo
