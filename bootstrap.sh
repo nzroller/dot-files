f93c499598b37cec75c69b6a64310bb623b8ad8b
Tim Lord <tim@izettle.com>
2016-03-04 14:09:26 +0100

    Squash warnings: be more ansible v2-esque

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 
 # Adds Ansible PPA manually (sort of what add-apt-repository does)
-tee /etc/apt/sources.list.d/ansible-ansible-trusty.list >/dev/null <<EOF
+tee /etc/apt/sources.list.d/ansible-ubuntu-ansible-trusty.list >/dev/null <<EOF
 deb http://ppa.launchpad.net/ansible/ansible/ubuntu trusty main
 # deb-src http://ppa.launchpad.net/ansible/ansible/ubuntu trusty main
 EOF
@@ -10,4 +10,4 @@ export ANSIBLE_LIBRARY=/usr/share/ansible
 EOF
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys "6125E2A8C77F2818FB7BD15B93C4A3FD7BB9C367"
 apt-get -y update
-apt-get -y install ansible unzip sudo
+apt-get -y install ansible
